f5060cc406e26fc4cfb9c9bea0392ab942f3393b
Roland Thompson <rolanditaru@gmail.com>
2020-09-13 10:18:55 +0000

    Finish install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-ln -s .bashrc ~/.bashrc
-ln -s .vimrc ~/.vimrc
+ln -nsf ~/dotfiles/.bashrc ~/.bashrc
+ln -nsf ~/dotfiles/.vimrc ~/.vimrc
 
 sudo apt-get update && sudo apt-get install -y \
     curl \
@@ -15,7 +15,17 @@ sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim
 
-source ~/.bashrc
-source ~/.vimrc
+mkdir -p ~/.config/nvim
+echo 'set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath=&runtimepath
+source ~/.vimrc' > ~/.config/nvim/init.vim
 
+# Install Plug (Vim plugin manager).
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Install FZF (fuzzy finder on the terminal and used by a Vim plugin).
+rm -rf ~/.fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+
+pip3 install pynvim jedi
