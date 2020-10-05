ef3c4daa6d93e045269ab28d204cde509236b6fd
Roland <rolanditaru@gmail.com>
2020-10-05 12:08:43 +1100

    Add ahk hotkeys and modify install script for latex

---
+++ ahk/hotkeys.ahk
@@ -0,0 +1,5 @@
+; Global hotkeys
+#t::Run, %UserProfile%\AppData\Local\Microsoft\WindowsApps\wt.exe
+#b::Run, %UserProfile%\AppData\Local\Programs\Batoner\Batoner.exe
+#s::Run, %UserProfile%\AppData\Local\slack\slack.exe
+#f::Run, firefox.exe
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 version=${1:-'simple'}
+latex=${2:-'no'}
 
 ln -nsf ~/dotfiles/.bashrc ~/.bashrc
 sudo apt-get update && sudo apt-get install -y \
@@ -32,6 +33,19 @@ if [ "$version" == "full" ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
     pip3 install pynvim jedi
+
+    if [ "$latex" == 'yes' ]; then
+        ln -nsf ~/dotfiles/vim/latex.vim ~/.vimrc
+
+        # Install tex live
+        echo "Installing TeX Live. This can take an hour."
+        curl https://mirror.aarnet.edu.au/pub/CTAN/systems/texlive/tlnet/install-tl-unx.tar.gz --output install-tl-unx.tar.gz
+        tar -xvzf install-tl-unx.tar.gz
+        cd *install-tl* && sudo perl install-tl
+
+        echo "Don't forget to set your PATH!"
+        echo "Install a PDF reader here: https://medium.com/@Pirmin/a-minimal-latex-setup-on-windows-using-wsl2-and-neovim-51259ff94734"
+    fi
 else
     ln -nsf ~/dotfiles/vim/simple.vim ~/.vimrc
 fi
