0af8ae7bb5edecc0f626a83f46c92d0e06800775
Roland Thompson <rolanditaru@gmail.com>
2021-05-14 10:14:52 +1000

    Remove latex setup and install Plug/CoC extensions

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 version=${1:-'simple'}
-latex=${2:-'no'}
 
 ln -nsf ~/dotfiles/.bashrc ~/.bashrc
 sudo apt-get update && sudo apt-get install -y \
@@ -41,17 +40,7 @@ if [ "$version" == "full" ]; then
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
     sudo apt update && sudo apt install yarn
 
-    if [ "$latex" == 'yes' ]; then
-        sed -i '$ d' ~/.vimrc
-        echo 'source ~/dotfiles/vim/latex.vim' >> ~/.vimrc
-
-        # Install tex live
-        echo "Installing TeX Live. This can take an hour."
-        curl https://mirror.aarnet.edu.au/pub/CTAN/systems/texlive/tlnet/install-tl-unx.tar.gz --output install-tl-unx.tar.gz
-        tar -xvzf install-tl-unx.tar.gz
-        cd *install-tl* && sudo perl install-tl
-
-        echo "Don't forget to set your PATH!"
-        echo "Install a PDF reader here: https://medium.com/@Pirmin/a-minimal-latex-setup-on-windows-using-wsl2-and-neovim-51259ff94734"
-    fi
+    # Install vim extensions
+    nvim --headless +PlugInstall +qall
+    nvim --headless +'CocInstall coc-python' +qall
 fi
