7e2cc91125e2234372db6c5ad8ff6f635efa95ae
Roland Thompson <rolanditaru@gmail.com>
2025-03-08 13:18:12 +1000

    Revert "Install command-not-found"
    This reverts commit 1a6107ed35cee0a3c0510c2e783b4811427ffaca.

--- install.sh
+++ install.sh
@@ -20,28 +20,27 @@ curl https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh |
 
 # Install dependencies
 sudo apt update && sudo apt install -y \
-	curl \
-	gnupg \
-	git \
-	htop \
-	rsync \
-	tmux \
-	unzip \
-	neovim \
-	zsh \
-	ripgrep \
-	python3-pip \
-	zplug \
-	fontconfig \
-	ruby-full \
-	build-essential \
-	zlib1g-dev \
-	git-lfs \
-	pipx \
-	fd-find \
-	command-not-found \
+    curl \
+    gnupg \
+    git \
+    htop \
+    rsync \
+    tmux \
+    unzip \
+    neovim \
+    zsh \
+    ripgrep \
+    python3-pip \
+    zplug \
+    fontconfig \
+    ruby-full \
+    build-essential \
+    zlib1g-dev \
+    git-lfs \
+    pipx \
+    fd-find \
 	wslu
-
+    
 # Install Node v21
 curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash -
 sudo apt-get install -y nodejs
